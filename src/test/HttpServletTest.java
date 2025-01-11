2e45896c90c1a56073919369294330be5b61afae
chengmengd <chengmengd@isoftstone.com>
2025-01-12 00:41:00 +0800

    ServletContext接口

--- src/test/HttpServletTest.java
+++ src/test/HttpServletTest.java
@@ -15,6 +15,8 @@ import java.io.IOException;
  */
 public class HttpServletTest implements Servlet {
 
+    private transient ServletConfig servletConfig;
+
     /**
      * 只在第一次访问该Servlet的时候创建Servlet程序会调用
      */
@@ -27,6 +29,7 @@ public class HttpServletTest implements Servlet {
      */
     @Override
     public void init(ServletConfig servletConfig) throws ServletException {
+        this.servletConfig = servletConfig;
         // ServletConfig接口是Servlet程序的配置信息
         // ServletConfig类的三大作用
         // 1、可以获取Servlet程序的别名，也就是web.xml中的servlet-name标签值
@@ -79,7 +82,25 @@ public class HttpServletTest implements Servlet {
     }
 
     public void doGet() {
-        // 业务逻辑
+        /*
+            ServletContext接口表示Servlet上下文对象，一个web工程只有一个ServletContext对象实例。
+            ServletContext对象是一个域对象，是可以像Map一样存取数据的对象，这里的域指的是存取数据的操作范围。
+         */
+        ServletContext servletContext = this.servletConfig.getServletContext();
+        // ServletContext类的四个作用
+        // 1、获取web.xml中配置的上下文参数，也就是context-param的值
+        String username = servletContext.getInitParameter("username");
+        System.out.println("context-param参数username的值是：" + username);
+        // 2、获取当前项目的工程路径，格式：/工程路径
+        System.out.println("当前工程路径：" + servletContext.getContextPath());
+        // 3、获取工程部署后在服务器硬盘上的绝对路径
+        /*
+            / 被服务器解析地址为：http://ip:port/工程名/ 映射到IDEA代码的web目录
+         */
+        System.out.println("工程部署的路径是：" + servletContext.getRealPath("/"));
+        // 4、像Map一样存取数据
+        servletContext.setAttribute("key1", "value1");
+        System.out.println("servletContext中获取与数据key1的值是：" + servletContext.getAttribute("key1"));
     }
 
     public void doPost() {
